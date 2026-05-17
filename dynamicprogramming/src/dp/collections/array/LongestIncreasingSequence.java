af6b511c08d6e64da7e4a133da4c5ffed2a22e78
yiweishen1988 <yiweishen1988@gmail.com>
2026-05-17 22:06:36 +0200

    Longest Increasing sequence with binary search method

--- dynamicprogramming/src/dp/collections/array/LongestIncreasingSequence.java
+++ dynamicprogramming/src/dp/collections/array/LongestIncreasingSequence.java
@@ -5,10 +5,12 @@
 package dp.collections.array;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class LongestIncreasingSequence
 {
-    public static int GetLongestLength(int[] nums)
+    // Complexity O(N*N)
+    public static int getLongestLength(int[] nums)
     {
         int length = nums.length;
         int[] dp = new int[length];
@@ -27,5 +29,34 @@ public class LongestIncreasingSequence
         }
 
         return Arrays.stream(dp).max().getAsInt();
+    }
+
+    // Complexity O(NlogN)
+    public static int getLongestLengthWithBinarySearch(List<Integer> values) {
+        int n = values.size();
+        if(n<=1) return n;
+
+        int[] tails = new int[n];
+        for (int i = 0; i < tails.length; i++) {
+            tails[i]=Integer.MAX_VALUE;
+        }
+        int result = 0;
+        for (int value : values) {
+            int left = 0, right=n-1;
+            while(left<=right){
+                int mid=(left+right)>>1;
+                if(tails[mid]<value){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }
+
+            if(left==n) return n;
+            tails[left]=value;
+            result=Math.max(result, left+1);
+        }
+
+        return result;
     }
 }
